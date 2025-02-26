c6868a3654ee3f1a1b227f74c9ce117aa65bb3b5
1Kazzan <kauancarneiro034@gmail.com>
2025-02-26 18:36:40 -0300

    passagem do código para o repositório e revisão

--- projeto.c
+++ projeto.c
@@ -1,15 +1,13 @@
-#include "pico/stdlib.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include "hardware/pwm.h"
-#include "hardware/pio.h"
-#include "hardware/clocks.h"
-#include "hardware/adc.h"
-#include "inc/ssd1306.h"
-#include "inc/font.h"
+#include "pico/stdlib.h"  // Inclui a biblioteca padrão para trabaçhar com a raspberry pi pico.
+#include <stdlib.h> // biblioteca padrão da linguagem C.
+#include <stdio.h> // Inclui a biblioteca padrão de entrada/saída para funções como printf e scanf.
+#include "hardware/pio.h" // Biblioteca para programar o PIO e utilizar a matriz de leds.
+#include "hardware/clocks.h" // Inclui a biblioteca de hardware para configuração de clocks.
+#include "hardware/adc.h"  // Inclui a biblioteca para controle do adc.
+#include "inc/ssd1306.h" // Inclui a biblioteca para controle do display6.
+#include "inc/font.h"  // Inclui a biblioteca de fontes para exibição de texto no display.
 
-//inclusão do arquivo pio
-#include "projeto.pio.h"
+#include "projeto.pio.h" //inclusão do arquivo pio
 
 // display
 #define I2C_PORT i2c1
@@ -21,15 +19,15 @@
 #define NUM_PIXELS 25 //quantidade de leds na matriz
 #define OUT_PIN 7 //gpio da matriz de leds
 
-// Pinos dos leds e botões
+// Pinos leds e botões
 #define sinal_verde 11
 #define sinal_azul 12
 #define sinal_vermelho 13
-#define sensor_alto 5 // gpio do botão 1
-#define sensor_baixo 6 // gpio do botão 2
-#define sensor_neutro 22 // GPIO para botão do Joystick
+#define sensor_alto 5 // simula sensor para ativat o modo para o trânsito alto.
+#define sensor_baixo 6 // simula sensor para ativat o modo para o trânsito baixo.
+#define sensor_neutro 22 // simula sensor para ativat o modo para o trânsito notmal.
 
-// Estados do semáforo
+// Estados do trânsito
 #define TRAFEGO_BAIXO 1
 #define TRAFEGO_NEUTRO 2
 #define TRAFEGO_ALTO 3
@@ -39,14 +37,14 @@ volatile uint32_t last_time_A = 0;
 volatile uint32_t last_time_B = 0;
 volatile uint32_t last_time_N = 0;
 
-int estado_atual = 2;
+int estado_atual = 2;// variável para mudar o estado de opereção do semáforo 
 
 
 // Função de interrupção com debouncing
 void gpio_irq_handler(uint gpio, uint32_t events) {
     uint32_t current_time = to_us_since_boot(get_absolute_time());
 
-    // Verifica o debounce para o sensor alto
+    // Verifica o debounce para o sensor alto(butão A).
     if (gpio == sensor_alto && current_time - last_time_A > 200000) { // 200 ms de debounce
         last_time_A = current_time; // muda o último estado para atual    
         // Ação para o botão A
@@ -54,7 +52,7 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
             estado_atual = TRAFEGO_ALTO; // Muda para o estado alto  
         }
     }
-    // Verifica o debounce para o sensor baixo
+    // Verifica o debounce para o sensor baixo(botão B).
     if (gpio == sensor_baixo && current_time - last_time_B > 200000) {
         last_time_B = current_time; // muda o último estado para atual        
         // Ação para o botão B
@@ -62,6 +60,7 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
             estado_atual = TRAFEGO_BAIXO; // Muda para o estado baixo
         }
     }
+    // Verifica o debounce para o sensor neuro.
     if (gpio == sensor_neutro && current_time - last_time_N > 200000) {
         last_time_N = current_time; // muda o último estado para atual        
         // Ação para o botão B
@@ -81,6 +80,7 @@ uint32_t matrix_rgb(double b, double r, double g)
   return (G << 24) | (R << 16) | (B << 8);
 }
 
+//Animação do Trânsito com fluxo normal.
 void trafego_neutro(uint32_t valor_led, PIO pio, uint sm) {
     double frames[10][25][3] = {
         {{0.0, 0.3, 0.3}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.5}, {0.5, 0.0, 0.0}, {0.0, 0.3, 0.3},
@@ -144,7 +144,7 @@ void trafego_neutro(uint32_t valor_led, PIO pio, uint sm) {
        {0.0, 0.3, 0.3}, {0.0, 0.0, 0.5}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.3, 0.3}},
     };
 
-    int delays[10] = {2000, 2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000, 2000};
+    int delays[10] = {2000, 2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000, 2000};// Delays entre as animação.
 
 for (int frame = 0; frame < 10; frame++) {
     for (int16_t i = 0; i < NUM_PIXELS; i++) {
@@ -154,26 +154,26 @@ for (int frame = 0; frame < 10; frame++) {
         valor_led = matrix_rgb(r, g, b);
         pio_sm_put_blocking(pio, sm, valor_led);
     }
-    // Controla os LEDs do semáforo com base no frame
+    // Controla os leds do semáforo com base no frame
     if (frame == 0 || frame == 6 || frame == 7 || frame == 8 || frame == 9) {  
-        gpio_put(sinal_verde, 1);   // Verde aceso
+        gpio_put(sinal_verde, 1);   
         gpio_put(sinal_vermelho, 0);
         gpio_put(sinal_azul, 0);
     } else if (frame == 1) {
         gpio_put(sinal_verde, 1);
         gpio_put(sinal_vermelho, 1);
-        gpio_put(sinal_azul, 0);    // Amarelo aceso
+        gpio_put(sinal_azul, 0);    
     } else {
         gpio_put(sinal_verde, 0);
-        gpio_put(sinal_vermelho, 1); // Vermelho aceso
+        gpio_put(sinal_vermelho, 1); 
         gpio_put(sinal_azul, 0);
     }
 
-    sleep_ms(delays[frame]);  // Usa o delay específico para cada frame
+    sleep_ms(delays[frame]);  // Usa o delay específico para cada frame para sincronizar a ação do semáforo com a animação.
 }
 };
 
-
+//Animação do Trânsito com fluxo baixo.
 void trafego_baixo(uint32_t valor_led, PIO pio, uint sm) {
     double frames[10][25][3] = {
         {{0.0, 0.3, 0.3}, {0.0, 0.0, 0.0}, {0.0, 0.5, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.3, 0.3},
@@ -237,7 +237,7 @@ void trafego_baixo(uint32_t valor_led, PIO pio, uint sm) {
        {0.0, 0.3, 0.3}, {0.0, 0.0, 0.0}, {0.0, 0.5, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.3, 0.3}},
     };
 
-    int delays[10] = {2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000, 2000, 2000};
+    int delays[10] = {2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000, 2000, 2000}; // Delays entre as animação.
 
 for (int frame = 0; frame < 10; frame++) {
     for (int16_t i = 0; i < NUM_PIXELS; i++) {
@@ -249,23 +249,22 @@ for (int frame = 0; frame < 10; frame++) {
     }
     // Controla os LEDs do semáforo com base no frame
     if (frame == 0 || frame == 1 || frame == 2 || frame == 3 || frame == 8 || frame == 9) {  
-        gpio_put(sinal_verde, 1);   // Verde aceso
+        gpio_put(sinal_verde, 1);   
         gpio_put(sinal_vermelho, 0);
         gpio_put(sinal_azul, 0);
     } else if (frame == 4) {
         gpio_put(sinal_verde, 1);
         gpio_put(sinal_vermelho, 1);
-        gpio_put(sinal_azul, 0);    // Amarelo aceso
+        gpio_put(sinal_azul, 0);    
     } else {
         gpio_put(sinal_verde, 0);
-        gpio_put(sinal_vermelho, 1); // Vermelho aceso
-        gpio_put(sinal_azul, 0);
+        gpio_put(sinal_vermelho, 1); 
     }
 
-    sleep_ms(delays[frame]);  // Usa o delay específico para cada frame
+    sleep_ms(delays[frame]); // Usa o delay específico para cada frame para sincronizar a ação do semáforo com a animação.
 }
 };
-            
+//Animação do Trânsito com fluxo alto.            
 void trafego_alto(uint32_t valor_led, PIO pio, uint sm) {
     double frames[11][25][3] = {
         {{0.0, 0.3, 0.3}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.5}, {0.5, 0.0, 0.0}, {0.0, 0.3, 0.3},
@@ -335,7 +334,7 @@ void trafego_alto(uint32_t valor_led, PIO pio, uint sm) {
        {0.0, 0.3, 0.3}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.3, 0.3}},
     };
 
-    int delays[11] = {2000, 2000, 2000, 2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000};
+    int delays[11] = {2000, 2000, 2000, 2000, 2000, 2000, 2000, 1500, 2000, 2000, 2000}; // Delays entre as animação.
 
 for (int frame = 0; frame < 11; frame++) {
     for (int16_t i = 0; i < NUM_PIXELS; i++) {
@@ -360,10 +359,11 @@ for (int frame = 0; frame < 11; frame++) {
         gpio_put(sinal_azul, 0);
     }
 
-    sleep_ms(delays[frame]);  // Usa o delay específico para cada frame
+    sleep_ms(delays[frame]); // Usa o delay específico para cada frame para sincronizar a ação do semáforo com a animação.
 }
 
 }
+// Configura a clock
 void inicializar_clock() {
     bool ok = set_sys_clock_khz(128000, false);
     if (ok) {
@@ -380,21 +380,21 @@ void configurar_pio(PIO pio, uint *offset, uint *sm) {
     projeto_program_init(pio, *sm, *offset, OUT_PIN);
 }
 
-void atualizar_display(ssd1306_t *ssd){
+void atualizar_display(ssd1306_t *ssd){  //função para atualizar o display de acordo com o estado atual do trânsito.
         // Limpa o display
         ssd1306_fill(ssd, 0);
 
     // Verifica o estado do trânsito e exibe a mensagem correspondente
     if (estado_atual == TRAFEGO_NEUTRO) {
-        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0); // Desenha um retângulo
+        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0); // retângulo
         ssd1306_draw_string(ssd, "FLUXO NORMAL", 20, 30); 
     } 
     else if (estado_atual == TRAFEGO_ALTO) {
-        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0);
+        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0); // retângulo
         ssd1306_draw_string(ssd, "FLUXO ALTO", 20, 30);
     } 
     else if (estado_atual == TRAFEGO_BAIXO) {
-        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0);
+        ssd1306_rect(ssd, 3, 3, 122, 58, 1, 0); // retângulo
         ssd1306_draw_string(ssd, "FLUXO BAIXO", 20, 30); 
     }
         // Atualiza o display
@@ -423,15 +423,15 @@ int main() {
 
     gpio_init(sensor_alto);
     gpio_set_dir(sensor_alto, GPIO_IN);
-    gpio_pull_up(sensor_alto); // Habilita o resistor de pull-up
+    gpio_pull_up(sensor_alto); 
 
     gpio_init(sensor_baixo);
     gpio_set_dir(sensor_baixo, GPIO_IN);
-    gpio_pull_up(sensor_baixo); // Habilita o resistor de pull-up
+    gpio_pull_up(sensor_baixo); 
 
     gpio_init(sensor_neutro);
     gpio_set_dir(sensor_neutro, GPIO_IN);
-    gpio_pull_up(sensor_neutro); // Habilita o resistor de pull-up
+    gpio_pull_up(sensor_neutro); 
     
     // Inicia o I2C com 400Khz.
     i2c_init(I2C_PORT, 400 * 1000);
